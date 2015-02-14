0f7cca9ae3c8e9a0162e049f2b654e8a9f6404bc
Kuijpers <darkknightworks@gmail.com>
2015-02-14 22:15:58 +0100

    index.php changed to start up file

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+error_reporting(E_ALL);
 
-echo "Dit is Horsey CMS";
+//echo "DIT IS DE INDEX PAGINA";
+
+require 'config/db.php';
+require 'config/login.php';
+require 'config/mail.php';
+require 'config/security.php';
+require 'config/url.php';
+/**
+ * @todo Put this requirement somewhere else so it will not be called all the time
+ */
+require LIBS . 'Form/Val.php';
+
+/**
+ * Load classes from libs
+ */
+spl_autoload_register(function ($class) {
+   include LIBS . $class.".php";
+});
+
+$bootstrap = new Bootstrap();
+
+//Optional path settings
+    //$bootstrap->setControllerPath('c');
+    //$bootstrap->setModelPath('m');
+    //$bootstrap->_setDefaultFile('d.php');
+    //$bootstrap->_setErrorFile('d.php');
+$bootstrap->init();
